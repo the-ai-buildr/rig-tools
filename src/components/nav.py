5a4f15a3c3131fa1c7e2da33258119aad42aa5e1
the-ai-buildr <tyler@theaibuildr.com>
2026-03-16 07:27:19 +0000

    idk

--- src/components/nav.py
+++ src/components/nav.py
@@ -4,14 +4,14 @@ from .utils import horizontal_rule
 
 def nav_menu():
     with st.popover("Goto", type="secondary", use_container_width=True, width=200):
+        # st.page_link("pages/00_template.py", label="Template", icon="📝")
         st.page_link("pages/01_home.py", label="Home", icon="🏠")
         st.page_link("pages/03_projects.py", label="Projects", icon="📁")
-        # st.page_link("pages/00_template.py", label="Template", icon="📝")
         st.page_link("pages/02_digital_stamp.py", label="Digital Stamp", icon="📱")
 
 
 def page_nav(title_text="", icon=""):
-    col1, col2 = st.columns([0.80, 0.20], gap="small", vertical_alignment="center")
+    col1, col2 = st.columns([0.80, 0.20], gap="small", vertical_alignment="bottom")
     cont = st.container()
 
     with cont:
