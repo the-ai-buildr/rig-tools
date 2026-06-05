a87a05a77a8d79074f3c371f748e64af765fa123
the-ai-buildr <tyler@theaibuildr.com>
2026-06-05 03:19:10 +0000

    up

--- pages/settings.py
+++ pages/settings.py
@@ -3,8 +3,11 @@ from dash import html
 from dash_iconify import DashIconify
 import dash_mantine_components as dmc
 from utils import page_header
+from components.ui.metric_card import metric_card
+from utils import page_header
 
 dash.register_page(__name__, path="/settings")
+
 
 layout = dmc.Box([
         page_header(
@@ -12,6 +15,8 @@ layout = dmc.Box([
             action=dmc.Button(
                 "Create",
                 id="create-settings-btn",
+                leftSection=DashIconify(icon="tabler:plus", width=16),
+                variant="subtle",
             ),
         ),
     ],
